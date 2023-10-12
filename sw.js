6bcfe7487d0a0a6504c0c6327c1c3a6c12828020
Luenna <79160513+LuennaCL@users.noreply.github.com>
2023-10-12 11:29:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
 self.addEventListener("install", e => {
-	console.log("Installing sw");
-	});
+    console.log("Installing sw");
+	console.log("Caching resources..");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./src/manifest.json",
+                "./src/images/logo192.png"
+            ]);
+        })
+    );
+    console.log("Resources in cache. Done");
+})
